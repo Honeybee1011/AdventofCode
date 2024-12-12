93d52bd5a1eb73cfa37c7984c2799e7f3fd13107
Honeybee1011 <93639015+Honeybee1011@users.noreply.github.com>
2024-12-12 15:19:38 +0800

    Let me test my code ong

--- Avent of Code/2024/5/day5_1.py
+++ Avent of Code/2024/5/day5_1.py
@@ -1,6 +1,25 @@
 f1 = open('input5.txt')
 
-for line in f1:
-    pass
+rules = {}
+pages = []
 
+for line in f1:
+    line = line.strip()
+
+    if '|' in line:
+        line = line.split('|')
+        if line[0] not in rules:
+            rules.update(line[0], [line[1]])
+        else:
+            rules[line[0]].append(line[1])
+    
+    elif ',' in line:
+        line = line.split(',')
+
+        for num in line:
+            pages.append(int(num))
+    
 f1.close()
+
+print(rules)
+print(pages)
